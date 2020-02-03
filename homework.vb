21f5f851d0fdc43df9eb2df1c4a0db1970782842
MustafaAli564 <55464035+MustafaAli564@users.noreply.github.com>
2020-02-03 18:28:37 +0500

    Add files via upload

---
+++ homework.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Dim membershipfile As New IO.FileStream("D:\membership.txt", IO.FileMode.OpenOrCreate, IO.FileAccess.ReadWrite, IO.FileShare.None)
+        membershipfile.Close()
+        '--------------------------
+
+        Dim x As String
+        Dim y As System.IO.StreamReader
+        y = New IO.StreamReader("D:\membership.txt")
+        Do Until y.EndOfStream
+            x = y.ReadLine()
+            Console.WriteLine(x)
+        Loop
+        y.Close()
+        '--------------------------
+
+
+
+
+
+        Console.ReadKey()
+    End Sub
+
+End Module
